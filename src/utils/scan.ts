cbb83cf07993854d37a7df652c80927d62f6b19c
Chapliy Vladislav <vladislav.chapliy@altcraft.com>
2023-07-29 22:53:04 +0300

    init

--- src/utils/scan.ts
+++ src/utils/scan.ts
@@ -1,11 +1,11 @@
-import { spawn } from 'child_process';
+import { exec } from 'child_process';
 import * as CONFIG from '../../secrets.json';
 
 export function scan(fileName: string) {
+    const command = `scanimage --format=jpeg -d "${CONFIG.PRINTER_NAME}" > "/opt/net-printer/files/scans/${fileName}.jpg"`;
+    
     const promise = new Promise<void>((resolve) => {
-        const process = spawn(`scanimage --format=jpeg -d "${CONFIG.PRINTER_NAME}" > "/opt/net-printer/files/scans/${fileName}.jpg"`);
-
-        process.addListener('close', () => resolve());
+        exec(command, () => resolve());
     });
     
     return promise;
