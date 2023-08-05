b7c182f6fd9256e3117119faa2a909844bdadbb7
Chapliy Vladislav <vladislav.chapliy@altcraft.com>
2023-08-05 15:39:51 +0300

    init

--- src/utils/scan.ts
+++ src/utils/scan.ts
@@ -1,11 +1,10 @@
-import { join } from 'path';
 import { exec } from 'child_process';
 import * as CONFIG from '../../secrets.json';
+import { getScannedFilesPath } from './path';
 
 export function scan() {
     const fileName = `${Date.now()}.jpg`;
-    const scannedFilesDir = join(__dirname, '..', '..', 'files', 'scans');
-    const command = `scanimage --format=jpeg -d "${CONFIG.PRINTER_NAME}" > "${scannedFilesDir}/${fileName}"`;
+    const command = `scanimage --device-name=${CONFIG.PRINTER_NAME} --resolution=600 --progress > ${getScannedFilesPath()}/${fileName}`;
     
     const promise = new Promise<void>((resolve) => {
         exec(command, () => resolve());
