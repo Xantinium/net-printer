0234eec66246bcc3ef1ca7b753b64140fcb71706
Chapliy Vladislav <vladislav.chapliy@altcraft.com>
2023-08-05 20:00:20 +0300

    init

--- src/main.ts
+++ src/main.ts
@@ -4,5 +4,5 @@ import { AppModule } from './app.module';
 (async function() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
-  await app.listen(3000);
+  await app.listen(80);
 })();
