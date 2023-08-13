b94d852f66d627861c943bce0cc2dd9b664b8fe4
Chapliy Vladislav <vladislav.chapliy@altcraft.com>
2023-08-13 22:16:29 +0300

    init

--- src/utils/convert.ts
+++ src/utils/convert.ts
@@ -1,10 +1,12 @@
 import fs from 'node:fs';
-import * as ILovePDFApi from '@ilovepdf/ilovepdf-nodejs';
+// import * as ILovePDFApi from '@ilovepdf/ilovepdf-nodejs';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const ILovePDFApi = require('@ilovepdf/ilovepdf-nodejs');
 import ILovePDFFile from '@ilovepdf/ilovepdf-nodejs/ILovePDFFile';
 import * as CONFIG from '../../secrets.json';
 import { getPrintedFilesPath } from './path';
 
-const iLovePdfApi = new ILovePDFApi.default(CONFIG.ILOVEPDF_PUBLIC_KEY, CONFIG.ILOVEPDF_SECRET_KEY);
+const iLovePdfApi = new ILovePDFApi(CONFIG.ILOVEPDF_PUBLIC_KEY, CONFIG.ILOVEPDF_SECRET_KEY);
 
 async function convertDocxToPdf(fileName: string) {
     const task = iLovePdfApi.newTask('officepdf');
